5dc5769360f01e4a2d3bcc0dda82176980d1a9ba
MityaAndreevich <dmitry.logachev@icloud.com>
2022-01-19 10:54:44 +0300

    NetworkManager added

---
+++ ContactsAlamofire/Services/NetworkManager.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkManager.swift
+//  ContactsAlamofire
+//
+//  Created by Dmitry Logachev on 19.01.2022.
+//
+
+import Foundation
+import Alamofire
+
+enum Link: String {
+    case linkFor50 = "https://randomuser.me/api/?results=50"
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetchDataWithAlamofire( _ url: String, completion: @escaping(Result<[Contact], NetworkError>) -> Void) {
+        AF.request(Link.linkFor50.rawValue)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    guard let results = Contact.getRandomContacts(from: value) else { return }
+                    DispatchQueue.main.async {
+                        completion(.success(results))
+                    }
+                case .failure(let error):
+                    completion(.failure(.decodingError))
+                    print(error)
+                }
+            }
+    }
+    
+    func fetchImage(from url: String, completion: @escaping (Data) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    completion(data)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
