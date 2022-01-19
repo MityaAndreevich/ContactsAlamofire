768dbb57387a85d4b8badab77aa320d8f221d67f
MityaAndreevich <dmitry.logachev@icloud.com>
2022-01-19 10:40:51 +0300

    Initial Commit

---
+++ ContactsAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsAlamofire
+//
+//  Created by Dmitry Logachev on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
